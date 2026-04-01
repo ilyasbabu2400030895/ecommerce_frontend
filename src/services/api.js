a9c17602bf942ceb77e9a16b2a089ed1695badd5
ilyyy <2400030895@kluniversity.in>
2026-04-01 14:55:08 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecommercebackend-production-1936.up.railway.app"
+  baseURL: "https://ecommercebackend-production-1936.up.railway.app/api"
 });
 
 export default API;
